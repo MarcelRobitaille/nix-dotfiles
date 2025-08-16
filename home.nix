0a41bf05637d49991c58c54dd90cd3ceddd20c1f
Marcel Robitaille <marcelrobitaille11@gmail.com>
2025-08-16 10:24:21 +0200

    [zsh] Enable direnv

--- home.nix
+++ home.nix
@@ -2,4 +2,7 @@
   home.stateVersion = "24.05";
   home.username = "marcel";
   home.homeDirectory = "/home/marcel";
+
+  programs.direnv.enable = true;
+  programs.direnv.nix-direnv.enable = true;
 }
