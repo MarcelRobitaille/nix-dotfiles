6645b123029a6a8941f1773f509b4bee1a1e9122
Marcel Robitaille <marcelrobitaille11@gmail.com>
2025-09-27 14:46:54 +0200

    [tmux] Remove duplicate extra config

--- modules/tmux/tmux.nix
+++ modules/tmux/tmux.nix
@@ -8,13 +8,6 @@
     mouse = true;
     keyMode = "vi";
     escapeTime = 0;
-    extraConfig = ''
-          # Pane movement
-      bind-key -n M-h select-pane -L
-      bind-key -n M-j select-pane -D
-      bind-key -n M-k select-pane -U
-      bind-key -n M-l select-pane -R
-    '';
   };
 
   home.file.".tmux.conf".source = ./tmux.conf;
