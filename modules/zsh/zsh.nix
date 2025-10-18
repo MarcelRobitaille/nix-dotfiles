3d56d70478ed29c63fa18e799533f0a037a414c9
Marcel Robitaille <marcelrobitaille11@gmail.com>
2025-10-19 01:20:48 +0200

    [zsh] Fix forgit and initExtra

--- modules/zsh/zsh.nix
+++ modules/zsh/zsh.nix
@@ -2,7 +2,16 @@
   programs.zsh = {
     enable = true;
     enableCompletion = true;
-  };
 
-  home.file.".zshrc".source = ./zshrc;
+    plugins = [
+      {
+        name = "zsh-forgit";
+        src = pkgs.zsh-forgit;
+        file = "share/zsh/zsh-forgit/forgit.plugin.zsh";
+      }
+    ];
+    # TODO: Split up ./zshrc into modules and source them from inlined initExtra
+    # Don't replace .zshrc exactly with ./zshrc. It breaks plugins and such.
+    initExtra = builtins.readFile ./zshrc;
+  };
 }
