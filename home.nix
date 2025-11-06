d4f91e24ee732be4d3f0deda60333b135932a71e
Marcel Robitaille <marcelrobitaille11@gmail.com>
2025-11-06 22:12:36 +0100

    [zsh] Fix LS_COLORS to fix low contrast folders

--- home.nix
+++ home.nix
@@ -2,6 +2,10 @@
   home.stateVersion = "24.05";
   home.username = "marcel";
   home.homeDirectory = "/home/marcel";
+  home.sessionVariables = {
+    # Fix 777 directories being unreadable blue on green
+    LS_COLORS = "di=34:ow=33:tw=37"; # directories blue, world-writable yellow, sticky white
+  };
 
   programs.direnv.enable = true;
   programs.direnv.nix-direnv.enable = true;
