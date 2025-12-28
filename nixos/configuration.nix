fe37ca9febb94bc71bc40d3126eb96304a6eb5db
Marcel Robitaille <marcelrobitaille11@gmail.com>
2025-12-28 23:56:20 +0100

    [nixos] Switch from sv_SE to en_SE locale

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -54,8 +54,21 @@ in
     LC_NUMERIC = "en_US.UTF-8";
     LC_PAPER = "en_US.UTF-8";
     LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "sv_SE.UTF-8"; # Use ISO 8601 time format
+    LC_TIME = "en_SE.UTF-8"; # Use ISO 8601 time format
   };
+
+  i18n.glibcLocales = (pkgs.glibcLocales.override {
+    allLocales = false;
+    locales = config.i18n.supportedLocales;
+  }).overrideAttrs (old: {
+    postUnpack = /* sh */ ''
+      cp ${pkgs.fetchurl {
+        url = "https://gist.githubusercontent.com/bmaupin/d64368e78cd359d309685fecbe2baf23/raw/e933a0fc2e727aa640f4a1cb1f699622876940fc/en_SE";
+        hash = "sha256-ArXL+rMDVWI4Mmcw8xoRlZFXhEeSQL0tLJf5pKEOx3s=";
+      }} $sourceRoot/localedata/locales/en_SE
+      echo 'en_SE.UTF-8/UTF-8 \' >> $sourceRoot/localedata/SUPPORTED
+    '';
+  });
 
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
